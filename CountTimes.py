8ffaf05c47b3016d6cbec56e6f0016b482131edc
wangtao <wangtao502@hotmail.com>
2018-11-19 18:29:33 +0800

    end

--- CountTimes.py
+++ CountTimes.py
@@ -24,17 +24,29 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         self.btn = tk.Button(self)
-        self.btn["text"] = "点击计数一次(" + str(self.countTimes) + ")"
+        self.btn["height"] = 2
+        self.btn["text"] = "  点击计数一次  "
         self.btn["command"] = self.say_hi
-        self.btn.pack(side="top")
+        self.btn.pack(side="left",pady=10)
+
+        self.lable = tk.Label(self)
+        self.lable['text'] = str(self.countTimes)
+        self.lable['foreground']="#FF0000"
+        self.lable['padx']=10
+        self.lable['font']=("微软雅黑", 25)
+        self.lable.pack(side="right",pady=10)
 
     def say_hi(self):
         self.countTimes += 1
         open(self.filePath, mode="w").write(str(self.countTimes))
         self.f.flush()
-        self.btn["text"] = "点击计数一次(" + str(self.countTimes) + ")"
+        # self.btn["text"] = "点击计数一次(" + str(self.countTimes) + ")"
+        self.lable['text'] = str(self.countTimes)
 
 
 root = tk.Tk()
+root.title("计数程序")
+root.geometry('320x480')
+
 app = Application(master=root)
 app.mainloop()
